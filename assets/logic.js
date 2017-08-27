105a54bb8a1f5815360812caa2164aabc058bad4
Lisa Newhouse <lisanewhouse@icloud.com>
2017-08-26 19:45:08 -0500

    firebase initialize

--- assets/logic.js
+++ assets/logic.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
 
-///Trying out a login feature...not going so well
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBzW335U61ATVv2yKRT8g6WbW4AHApacOQ",
+    authDomain: "idlecare-786a2.firebaseapp.com",
+    databaseURL: "https://idlecare-786a2.firebaseio.com",
+    projectId: "idlecare-786a2",
+    storageBucket: "",
+    messagingSenderId: "231304815984"
+  };
+  firebase.initializeApp(config);
+
+
 
 
 
